74f917d99f20da7f4ae7b01c0094dfb929faf2aa
emjwivell <emjwivell@gmail.com>
2016-02-04 17:07:15 -0500

    Final

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+import random
+with open("/usr/share/dict/words") as my_file:
+    contents = my_file.read().splitlines()
+random_word = random.choice(contents)
 word = "ball"
 num_of_tries = 8
 
-empty_word = list("_" * len(word))
+empty_word = list("_" * len(random_word))
+x = len(random_word)
 print(empty_word)
+print("Your game contains " + str(x) + " amount of letters, good luck!")
 
 while num_of_tries > 0:
+
     if "_" not in empty_word:
         print("You are the WINNER!")
         break
+
     counter = 0
     guess = input("What is your guess? ")
-    for character in word:
+
+    for character in random_word:
+
         if character == guess:
             empty_word[counter] = guess
+
         counter += 1
 
-    if guess not in word:
+    if guess not in random_word:
         print("Wrong, try again!")
         num_of_tries -= 1
         print(str(num_of_tries) + " Guess(es) Remaining.")
 
-
-
     print(empty_word)
+print("Your word was: " + str(random_word))
